9429887ac949ba7691f8b20de60d07df12b04681
Allô Chéri <120775911+SottoVoce91@users.noreply.github.com>
2026-09-16 19:44:50 +0700

    Create gallery.js

---
+++ js/gallery.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.addEventListener("contextmenu", event => {
+        if (event.target.tagName === "IMG") {
+            event.preventDefault();
+        }
+    });
+
+    document.addEventListener("dragstart", event => {
+        if (event.target.tagName === "IMG") {
+            event.preventDefault();
+        }
+    });
+
+});
